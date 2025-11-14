e81a5bbefecb1afd7eee090ce99dd6d518e6383f
Amal Bilal <amalbilal20@gmail.com>
2025-11-14 20:06:08 +0000

    complete script parsing functions successfully!

--- src/hw7.c
+++ src/hw7.c
@@ -234,12 +234,183 @@ char* infix2postfix_sf(char *infix) {
     return postfix;
 }
 
+struct matrix_node {
+    matrix_sf *mat;
+    struct matrix_node* next;
+};
+
+struct matrix_node *mat_push(struct matrix_node *stack, matrix_sf *mat) {
+    struct matrix_node *new_node = malloc(sizeof(struct matrix_node));
+    if (new_node == NULL) {
+        return NULL;
+    }
+    new_node -> mat = mat;
+    new_node -> next = stack;
+    return new_node;
+}
+
+struct matrix_node *mat_pop(struct matrix_node *stack, matrix_sf **mat) {
+    if (stack == NULL) {
+        return NULL;
+    }
+    struct matrix_node *top = stack;
+    *mat = top -> mat;
+    struct matrix_node *new_top = top -> next;
+    free(top);
+    return new_top;
+}
+
 matrix_sf* evaluate_expr_sf(char name, char *expr, bst_sf *root) {
-    return NULL;
+    char *postfix = infix2postfix_sf(expr);
+    if (postfix == NULL) {
+        return NULL;
+    }
+    struct matrix_node *stack = NULL;
+    for (int i = 0; i < strlen(postfix); i++) {
+        char c = postfix[i];
+        if (c == ' ') {
+            continue;
+        }
+        else if (isalpha(c)) {
+            matrix_sf *mat = find_bst_sf(c, root);
+            stack = mat_push(stack, mat);
+        }
+        else if (c == '\'') {
+            matrix_sf *mat;
+            stack = mat_pop(stack, &mat);
+            matrix_sf *result = transpose_mat_sf(mat);
+            result -> name = '!';
+            stack = mat_push(stack, result);
+            if (!isalpha(mat -> name)) {
+                free(mat);
+            }
+        }
+        else if (c == '*') {
+            matrix_sf *mat2;
+            matrix_sf *mat1;
+            stack = mat_pop(stack, &mat2);
+            stack = mat_pop(stack, &mat1);
+            matrix_sf *result = mult_mats_sf(mat1, mat2);
+            result -> name = '!';
+            stack = mat_push(stack, result);
+            if (!isalpha(mat1 -> name)) {
+                free(mat1);
+            }
+            if (!isalpha(mat2 -> name)) {
+                free(mat2);
+            }
+        }
+        else if (c == '+') {
+            matrix_sf *mat1;
+            matrix_sf *mat2;
+            stack = mat_pop(stack, &mat1);
+            stack = mat_pop(stack, &mat2);
+            matrix_sf *result = add_mats_sf(mat1, mat2);
+            result -> name = '!';
+            stack = mat_push(stack, result);
+            if (!isalpha(mat1 -> name)) {
+                free(mat1);
+            }
+            if (!isalpha(mat2 -> name)) {
+                free(mat2);
+            }
+        }
+    }
+    matrix_sf *evaluated;
+    stack = mat_pop(stack, &evaluated);
+    evaluated -> name = name;
+    free(postfix);
+    return evaluated;
+}
+
+void free_bst_sf_and_keep_result(bst_sf *root, matrix_sf *result) {
+    if (root == NULL) {
+        return;
+    }
+    free_bst_sf_and_keep_result(root->left_child, result);
+    free_bst_sf_and_keep_result(root->right_child, result);
+    if ((root -> mat) != result) {
+        free(root -> mat);
+    }
+    free(root);
 }
 
 matrix_sf *execute_script_sf(char *filename) {
-   return NULL;
+    char *str = NULL;
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        return NULL;
+    }
+    size_t max_line_size = MAX_LINE_LEN;
+    bst_sf *root = NULL;
+    matrix_sf *matrix = NULL;
+    while (getline(&str, &max_line_size, file) != -1) { // loop through each line
+        if (str == NULL) {
+            continue;
+        }
+        size_t length = strlen(str);
+        if (length > 0 && str[length-1] == '\n') { // skip newline character
+            str[length-1] = '\0';
+        }
+        int is_short = 0;
+        for (int i = 0; i < strlen(str); i++) {
+            if (str[i] != ' ') {
+                is_short = 1;
+                break;
+            }
+        }
+        if (!is_short) { // skip empty lines
+            free(str);
+            str = NULL;
+            continue;
+        }
+        char mat_name = '\0';
+        int index = 0;
+        while (str[index] != '\0' && str[index] == ' ') {
+            index++;
+        }
+        if (str[index] != '\0' && isalpha(str[index])) {  // check if matrix name is a letter
+            mat_name = str[index];
+        }
+        char *equal_ptr = strchr(str, '=');
+        if (equal_ptr == NULL || mat_name == '\0') { // invalid line
+            free(str);
+            str = NULL;
+            continue;
+        }
+        equal_ptr++;
+        while (*equal_ptr != '\0' && *equal_ptr == ' ') {
+            equal_ptr++;
+        }
+        int is_matrix_definition = 0;
+        if (strchr(equal_ptr, '[') != NULL) {
+            is_matrix_definition = 1;
+        }
+        if (is_matrix_definition) { // line is a matrix definition
+            matrix_sf *new_matrix = create_matrix_sf(mat_name, equal_ptr);
+            if (new_matrix != NULL) {
+                root = insert_bst_sf(new_matrix, root);
+                matrix = new_matrix;
+            }
+        }
+        else { // line must be an expression
+            matrix_sf *evaluated_matrix = evaluate_expr_sf(mat_name, equal_ptr, root);
+            if (evaluated_matrix != NULL) {
+                root = insert_bst_sf(evaluated_matrix, root);
+                matrix = evaluated_matrix;
+            }
+        }
+        free(str);
+        str = NULL;
+    }
+    if (str != NULL) {
+        free(str);
+    }
+    fclose(file);
+    if (root != NULL) {
+        free_bst_sf_and_keep_result(root, matrix);
+    }
+    return matrix;
 }
 
 // This is a utility function used during testing. Feel free to adapt the code to implement some of
